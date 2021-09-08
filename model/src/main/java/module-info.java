913cb24c941e365a8e4be2f27214f42364c47d04
Sergio Martin Rubio <econsergio@gmail.com>
2021-09-08 15:47:59 +0200

    Specify module boundary

--- model/src/main/java/module-info.java
+++ model/src/main/java/module-info.java
@@ -1,9 +1,7 @@
 module com.sergiomartinrubio.model {
     requires com.sergiomartinrubio.persistence;
 
-    requires spring.boot;
-    requires spring.boot.autoconfigure;
     requires spring.context;
 
-    exports com.sergiomartinrubio.model.service;
+    exports com.sergiomartinrubio.model.service to com.sergiomartinrubio.api;
 }
