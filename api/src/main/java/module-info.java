f5457b74ecc637c78c1f17057cbac4c97b206916
Sergio Martin Rubio <econsergio@gmail.com>
2021-09-08 15:32:59 +0200

    Clean up

--- api/src/main/java/module-info.java
+++ api/src/main/java/module-info.java
@@ -1,9 +1,8 @@
 module com.sergiomartinrubio.api {
     requires com.sergiomartinrubio.model;
+    requires com.sergiomartinrubio.persistence;
 
     requires spring.web;
-    requires com.fasterxml.jackson.databind;
-    requires com.sergiomartinrubio.persistence;
 
     exports com.sergiomartinrubio.api;
 }
